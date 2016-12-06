392810b3f785637d14ae9539ba88ec2ba0fe0a38
muukii <m@muukii.me>
2016-12-06 20:57:17 +0900

    Add ZoomMode

--- ZoomImageView/ZoomImageView.swift
+++ ZoomImageView/ZoomImageView.swift
@@ -21,11 +21,23 @@
 // THE SOFTWARE.
 
 import UIKit
-import AVFoundation
 
 open class ZoomImageView: UIScrollView, UIScrollViewDelegate {
 
+  public enum ZoomMode {
+    case fit
+    case fill
+  }
+
+  // MARK: - Properties
+
   private let imageView = UIImageView()
+
+  public var zoomMode: ZoomMode = .fit {
+    didSet {
+      updateImageView()
+    }
+  }
 
   open var image: UIImage? {
     get {
@@ -39,6 +51,8 @@ open class ZoomImageView: UIScrollView, UIScrollViewDelegate {
     }
   }
 
+  // MARK: - Initializers
+
   public override init(frame: CGRect) {
     super.init(frame: frame)
     setup()
@@ -48,6 +62,8 @@ open class ZoomImageView: UIScrollView, UIScrollViewDelegate {
     super.init(coder: aDecoder)
     setup()
   }
+
+  // MARK: - Functions
 
   open func setup() {
 
@@ -95,8 +111,48 @@ open class ZoomImageView: UIScrollView, UIScrollViewDelegate {
   private var oldSize: CGSize?
 
   private func updateImageView() {
+
+    func fitSize(aspectRatio: CGSize, boundingSize: CGSize) -> CGSize {
+
+      let widthRatio = (boundingSize.width / aspectRatio.width)
+      let heightRatio = (boundingSize.height / aspectRatio.height)
+
+      var boundingSize = boundingSize
+
+      if widthRatio < heightRatio {
+        boundingSize.height = boundingSize.width / aspectRatio.width * aspectRatio.height
+      }
+      else if (heightRatio < widthRatio) {
+        boundingSize.width = boundingSize.height / aspectRatio.height * aspectRatio.width
+      }
+      return CGSize(width: ceil(boundingSize.width), height: ceil(boundingSize.height))
+    }
+
+    func fillSize(aspectRatio: CGSize, minimumSize: CGSize) -> CGSize {
+      let widthRatio = (minimumSize.width / aspectRatio.width)
+      let heightRatio = (minimumSize.height / aspectRatio.height)
+
+      var minimumSize = minimumSize
+
+      if widthRatio > heightRatio {
+        minimumSize.height = minimumSize.width / aspectRatio.width * aspectRatio.height
+      }
+      else if (heightRatio > widthRatio) {
+        minimumSize.width = minimumSize.height / aspectRatio.height * aspectRatio.width
+      }
+      return CGSize(width: ceil(minimumSize.width), height: ceil(minimumSize.height))
+    }
+
     guard let image = imageView.image else { return }
-    var size = AVMakeRect(aspectRatio: image.size, insideRect: bounds).size
+
+    var size: CGSize
+
+    switch zoomMode {
+    case .fit:
+      size = fitSize(aspectRatio: image.size, boundingSize: bounds.size)
+    case .fill:
+      size = fillSize(aspectRatio: image.size, minimumSize: bounds.size)
+    }
 
     size.height = round(size.height)
     size.width = round(size.width)
@@ -104,6 +160,7 @@ open class ZoomImageView: UIScrollView, UIScrollViewDelegate {
     maximumZoomScale = image.size.width / size.width
     imageView.bounds.size = size
     contentSize = size
+    imageView.center = contentCenter(forBoundingSize: bounds.size, contentSize: contentSize)
   }
 
   @objc private func handleDoubleTap() {
@@ -115,7 +172,6 @@ open class ZoomImageView: UIScrollView, UIScrollViewDelegate {
   }
 
   // MARK: - UIScrollViewDelegate
-
   public func scrollViewDidZoom(_ scrollView: UIScrollView) {
     imageView.center = contentCenter(forBoundingSize: bounds.size, contentSize: contentSize)
   }
@@ -140,10 +196,9 @@ open class ZoomImageView: UIScrollView, UIScrollViewDelegate {
     /// ratio, there will be some empty space horizontaly or verticaly. This needs to be calculated
     /// so that we can get the correct new center value. When these are added, edges of contentView
     /// are aligned in realtime and always aligned with corners of scrollview.
-
     let horizontalOffest = (boundingSize.width > contentSize.width) ? ((boundingSize.width - contentSize.width) * 0.5): 0.0
     let verticalOffset = (boundingSize.height > contentSize.height) ? ((boundingSize.height - contentSize.height) * 0.5): 0.0
-
+    
     return CGPoint(x: contentSize.width * 0.5 + horizontalOffest,  y: contentSize.height * 0.5 + verticalOffset)
   }
 }
